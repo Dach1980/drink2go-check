2320aeba66b4b6ad73f2183d69bc2f94864f21d8
Dmitry Cherkashin <dchpost@mail.ru>
2025-02-14 14:52:24 +1000

    корректирует секцию hero и добавляет слайдер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,25 @@
 /* в этот файл добавляет скрипты*/
+
+/* Swiper */
+const swiper = new Swiper(".hero-swiper", {
+  loop: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  speed: 1000,
+  pagination: {
+    el: ".hero-swiper__pagination",
+    bulletClass: "hero-swiper__pagination-bullet",
+    bulletActiveClass: "hero-swiper__pagination-bullet--active",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".hero-swiper__button--next",
+    prevEl: ".hero-swiper__button--prev",
+  },
+  a11y: {
+    prevSlideMessage: "Следующий слайд",
+    nextSlideMessage: "Предыдущий слайд",
+  },
+});
